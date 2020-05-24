b89345f106b1f6f68c5565ed35a660dd75a7ec5a
lorry-913 <377148953@qq.com>
2020-05-24 16:47:30 +0800

    增加依赖倒置原则

--- src/com/midea/designmodel/princple/DependcyInversion.java
+++ src/com/midea/designmodel/princple/DependcyInversion.java
@@ -7,6 +7,10 @@ package com.midea.designmodel.princple;
  * 2.构造方法传递
  * 3.setter方法传递
  *
+ * 小结:
+ *     1.底层模块尽量都要有抽象类或者接口
+ *     2.变量的声明类型尽量时抽象类或者接口，这样我们的对象引用就有一个缓冲层
+ *
  */
 public class DependcyInversion {
     public static void main(String[] args) {
