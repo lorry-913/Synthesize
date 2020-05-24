cb86c69ecc4844ebb403288b55e2bc384db6971f
lorry-913 <377148953@qq.com>
2020-05-24 18:09:26 +0800

    增加开闭原则解决方案

---
+++ src/com/midea/designmodel/princple/SolveOcp.java
@@ -0,0 +1,41 @@
+package com.midea.designmodel.princple;
+
+public class SolveOcp {
+    public static void main(String[] args) {
+        Draw1 draw1=new Draw1();
+        Sanjiaoxing1 sanjiaoxing1=new Sanjiaoxing1();
+        Zhengfangx1 zhengfangx1=new Zhengfangx1();
+        draw1.draw(sanjiaoxing1);
+        draw1.draw(zhengfangx1);
+    }
+}
+
+class Draw1{
+    void draw(Shape1 s){
+        s.draw();
+    }
+
+}
+
+abstract class Shape1{
+    int type;
+
+    abstract void draw();
+}
+
+class Sanjiaoxing1 extends Shape1{
+
+
+    @Override
+    void draw() {
+        System.out.println("绘制三角形");
+    }
+}
+
+class Zhengfangx1 extends Shape1{
+
+    @Override
+    void draw() {
+        System.out.println("绘制正方形");
+    }
+}
